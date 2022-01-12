0c74ae4825493657328ba7ded041146fffb50023
PatrikWithJustAK <patrik.gammon@gmail.com>
2022-01-12 18:27:48 -0500

    Mapped additional fields for transactions

--- InventoryEquipment/admin.py
+++ InventoryEquipment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from InventoryEquipment.models import Equipment, EquipmentInstance
+from InventoryEquipment.models import Equipment, EquipmentInstance, Transaction
 # Register your models here.
 admin.site.register(Equipment)
 admin.site.register(EquipmentInstance)
+admin.site.register(Transaction)
--- InventoryEquipment/models.py
+++ InventoryEquipment/models.py
@@ -30,6 +30,7 @@ class EquipmentInstance(models.Model):
         ('A', 'Active'),
         ('I', 'Inactive'),
     )
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     equipment = models.ForeignKey(Equipment, verbose_name=("Which type of equipment is this"), on_delete=models.CASCADE)
     serial_number = models.CharField(("Serial Number for this equipment"), max_length=50)
@@ -48,8 +49,31 @@ class EquipmentInstance(models.Model):
         return str(self.serial_number)
 
 class Transaction(models.Model):
+    LOCATION_STATUS = (
+        ('OH', 'On Hand'),
+        ('PT', 'Patient'),
+        ('RC', 'Repair Center'),
+        ('RT', 'Retired'),
+        ('MIA', 'Missing In Action'),
+    )
+    EQUIPMENT_STATUS = (
+        ('A', 'Active'),
+        ('I', 'Inactive'),
+    )
+
     
-    equipment_instance = models.ForeignKey(EquipmentInstance, verbose_name=("The equipment that this transaction is for"), on_delete=models.CASCADE, blank=True)
-    date_submitted = models.DateTimeField(_("When was this Transaction Submitted?"), auto_now=True, auto_now_add=True)
+    date_submitted = models.DateTimeField(_("When was this Transaction Submitted?"), auto_now_add=True)
     date_approved = models.DateTimeField(_("When was this Transaction Approved?"), auto_now=False, auto_now_add=False, blank=True, null=True)
-    serial_number = models.CharField(_("Serial Number for the associated Equipment"), max_length=50)
+    serial_number = models.ForeignKey(EquipmentInstance, verbose_name=("The equipment that this transaction is for"), on_delete=models.CASCADE, blank=True)
+    submitter= models.ForeignKey(CustomUser, related_name="requester", verbose_name=_(""), on_delete=models.CASCADE)
+    store_location = models.ForeignKey("InventoryStores.StoreLocation", verbose_name=("Location Equipment is currently at"), on_delete=models.CASCADE)
+    location_status = models.CharField(_("Location Status Of Equipment"), max_length=3, choices=LOCATION_STATUS, default='OH')
+    equipment_status = models.CharField(_("Equipment Status active/inactive/etc"), max_length=2, choices =EQUIPMENT_STATUS, default='A')
+    current_customer_id = models.CharField(_("Customer ID of the customer who currently has possession of equipment"), max_length=50)
+    meter_hours = models.PositiveIntegerField(_("Meter hours on the equipment"), null=True)
+    next_pm_date = models.DateField(_("Date for next maintenance"), auto_now=False, auto_now_add=False)
+    manager = models.ForeignKey(CustomUser, verbose_name=_("Manager responsible for this equipment"), on_delete=models.CASCADE)
+    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    
+    def __str__(self):
+        return str(self.transaction_id)
